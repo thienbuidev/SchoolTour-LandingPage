56403f18d841eb4cb3a08e679ba4f0bc3af3716b
bxthien <buixuanthien.0105@gmail.com>
2025-06-25 17:05:24 +0700

    update footer

--- src/pages/HomePage/FooterDevplus.tsx
+++ src/pages/HomePage/FooterDevplus.tsx
@@ -7,7 +7,7 @@ import { FaEnvelope, FaGlobe, FaPhone } from "react-icons/fa";
 
 const FooterDevplus = () => {
   return (
-    <Footer className="bg-orange-700 flex items-center justify-center max-w-screen-2xl md:!gap-20 lg:!gap-10 gap-12 !p-4">
+    <Footer className="bg-orange-700 flex items-center justify-center max-w-screen-2xl md:!gap-20 lg:!gap-10 gap-2 !p-4">
       <div className="md:p-4 p-0">
         <Image
           src={Logo}
